7e697be9bcfc41642422d632f0b3aa7e50ea978e
ArantxaDR <arantxaderuiz@gmail.com>
2020-10-23 10:50:15 +0200

    add grid & fix url

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 "use strict";
-let drama = [];
-fetch("http://api.tvmaze.com/search/shows?q=tronos")
-  .then((response) => {
-    return response.json();
-  })
-  .then(function (data) {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].show.name);
-      console.log(data[i].show.image.medium);
-    }
-  });
+
+let tvSeries = [];
+
+const allSeries = document.querySelector(".js-tvSeries");
+
+function getSeries() {
+  fetch("//api.tvmaze.com/search/shows?q=tronos")
+    .then((response) => {
+      return response.json();
+    })
+    .then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        tvSeries[0] = data[i].show.name;
+        tvSeries[1] = data[i].show.image.medium;
+        console.log(tvSeries);
+        paintSeries();
+      }
+    });
+}
+
+function paintSeries() {
+  console.log("pintar");
+}
+getSeries();
