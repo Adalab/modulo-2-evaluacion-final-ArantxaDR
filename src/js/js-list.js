5928952b454de274abb011781f675e23ece9538e
ArantxaDR <arantxaderuiz@gmail.com>
2020-10-23 09:50:00 +0200

    do fetch

--- src/js/js-list.js
+++ src/js/js-list.js
@@ -1,5 +1,1 @@
 "use strict";
-
-fetch("http://api.tvmaze.com/search/shows?q=tronos")
-  .then((response) => response.json())
-  .then((data) => (document.body.innerHTML = data.result));
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
-'use strict';
-
-console.log('>> Ready :)');
+"use strict";
+let drama = [];
+fetch("http://api.tvmaze.com/search/shows?q=tronos")
+  .then((response) => {
+    return response.json();
+  })
+  .then(function (data) {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].show.name);
+      console.log(data[i].show.image.medium);
+    }
+  });
