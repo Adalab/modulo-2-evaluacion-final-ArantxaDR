7a37c24f4a3a2035e0e72dfdb4df42fa180d63d1
ArantxaDR <arantxaderuiz@gmail.com>
2020-10-24 19:11:32 +0200

    add styles to fav

--- src/js/favList.js
+++ src/js/favList.js
@@ -8,11 +8,15 @@ function paintFav() {
   for (let i = 0; i < favList.length; i++) {
     showList += `<li class="js-fav-item js-fav${i} fav_list_item">`;
     showList += `<p>${favList[i].name}</p>`;
+
     if (favList[i].image === null) {
       showList += `<img  src= "//via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="Show TV image"/>`;
     } else {
       showList += `<img  src="${favList[i].image.medium}" alt="Show TV image"/>`;
     }
+    showList += `<button class="fav_list_delete js-delete-btn"  title="Borrar">`;
+    showList += `<i class="fav_list_delete_icon fas fa-times"></i>`;
+    showList += `</button>`;
     showList += "</li>";
   }
   favSeriesContainer.innerHTML = showList;
